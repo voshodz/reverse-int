4102d550074e9b691a0622033a53c0f222d0ab5f
voshodz <borcektrash@mail.ru>
2022-01-01 04:40:44 +0600

    feat: added function reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    str = n.toString()
+    res = ""
+    len = str.length
+    for(let i = 0;i < len;i++) {
+        res += str[len - i - 1] 
+    }
+    return(res.replace('-', ''))
 }
